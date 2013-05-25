09746b5506723e381953b1af027ceb5ab981a69b
Trevor Ewen <tewen@neosavvy.com>
2013-05-25 11:17:54 -0400

    Removed the checkin of the Intellij project files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biogasdata::Application.config.secret_token = '31b6cba335dfdcb989bd5a102916d4a077528a3ad93c52134b7e21201d78f9143e7178f01ceccd3f45d6843e95f1b7cf4072babfe7024d6dfac1fdfc89830418'
