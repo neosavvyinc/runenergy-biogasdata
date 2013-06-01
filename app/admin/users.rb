55e02b915deab49281d4b7be1101ce5b219eea8e
Trevor Ewen <tewen@neosavvy.com>
2013-06-01 15:34:55 -0400

    Cleaned up the customer creation interface in the admin.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,27 @@
 ActiveAdmin.register User, :as => "Customers/Viewers" do
   menu :parent => "Users"
+
+  index do
+    column :name
+    column :email
+    column :user_type
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  filter :name
+  filter :email
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :user_type,  :as => :select, :collection => UserType.all
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
