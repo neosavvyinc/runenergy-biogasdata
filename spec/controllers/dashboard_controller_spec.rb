00c8847026589fb6c5bf0f53ec6c7b4adf879ffb
Trevor Ewen <tewen@neosavvy.com>
2013-11-12 22:08:53 -0500

    Added the flare_deployements tests for the dashboard controller.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,13 +1,22 @@
-require "spec_helper"
+require 'spec_helper'
+require 'json'
 
 describe DashboardController do
 
   describe 'read_flare_deployments' do
 
+    let :jordan do
+      FactoryGirl.create(:user, :name => "Jordan Frankfurt", :user_type => UserType.CUSTOMER)
+    end
+
+    let :trevor do
+      FactoryGirl.create(:user, :name => "Trevor Ewen", :user_type => UserType.CUSTOMER)
+    end
+
     let :flare_deployments do
-      [FactoryGirl.create(:flare_deployment, :client_flare_unique_identifier => "Mike"),
-       FactoryGirl.create(:flare_deployment, :client_flare_unique_identifier => "Thomas"),
-       FactoryGirl.create(:flare_deployment, :client_flare_unique_identifier => "Robert")]
+      [FactoryGirl.create(:flare_deployment, :customer => jordan),
+       FactoryGirl.create(:flare_deployment, :customer => trevor),
+       FactoryGirl.create(:flare_deployment, :customer => trevor)]
     end
 
     before :each do
@@ -19,14 +28,27 @@ describe DashboardController do
 
       it 'should return all of the flare deployments if the user is not a customer' do
         xhr :get, :read_flare_deployments
-        response.should_not be_nil
+        parsed_response = JSON.parse response.body
+        parsed_response.size.should eq(3)
+        parsed_response[0]["customer_id"].should eq(jordan.id)
+        parsed_response[1]["customer_id"].should eq(trevor.id)
+        parsed_response[2]["customer_id"].should eq(trevor.id)
       end
     end
 
     describe 'customer' do
-      login_customer
+      before(:each) do
+        sign_in trevor
+      end
 
       it 'should return just the customers flare deployments if the user is a customer' do
+        xhr :get, :read_flare_deployments
+        parsed_response = JSON.parse response.body
+        parsed_response.size.should eq(2)
+        parsed_response[0]["id"].should eq flare_deployments[1].id
+        parsed_response[0]["customer_id"].should eq(trevor.id)
+        parsed_response[1]["id"].should eq flare_deployments[2].id
+        parsed_response[1]["customer_id"].should eq(trevor.id)
       end
 
     end
