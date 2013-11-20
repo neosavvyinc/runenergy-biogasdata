019e0e27c0c58f4aace845279511f7674e9f36d7
Trevor Ewen <tewen@neosavvy.com>
2013-11-19 20:54:08 -0500

    Got the admin for monitor classes up and running.

--- app/admin/monitor_classes.rb
+++ app/admin/monitor_classes.rb
@@ -2,20 +2,30 @@ ActiveAdmin.register MonitorClass do
 
   menu :parent => 'Monitored Settings'
 
+  show do
+    panel "Monitor Points" do
+      table_for monitor_class.monitor_points do
+        column :name
+        column :unit
+      end
+    end
+    panel "Details" do
+      h6 "Created At: #{monitor_class.created_at}"
+      h6 "Updated At: #{monitor_class.updated_at}"
+    end
+  end
+
   form do |f|
     f.inputs 'Monitor Class' do
       f.input :name
-      f.has_many :monitor_classes_monitor_points do |app_f|
-        app_f.inputs "Monitor Points" do
-          if !app_f.object.nil?
-            # show the destroy checkbox only if it is an existing appointment
-            # else, there's already dynamic JS to add / remove new appointments
-            app_f.input :_destroy, :as => :boolean, :label => "Destroy?"
-          end
-
-          app_f.input :monitor_point # it should automatically generate a drop-down select to choose from your existing patients
-        end
-      end
+      f.input :monitor_points, :as => :select, :collection => MonitorPoint.all
+      #f.has_many :monitor_points do |mp|
+      #  mp.inputs "Monitor Points" do
+      #    mp.input :name
+      #    mp.input :unit
+      #    #repeat as necessary for all fields
+      #  end
+      #end
     end
     f.actions
   end
--- app/models/monitor_class.rb
+++ app/models/monitor_class.rb
@@ -1,6 +1,10 @@
 class MonitorClass < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :monitor_point_ids
   has_many :readings
   has_many :monitor_classes_monitor_points
   has_many :monitor_points, through: :monitor_classes_monitor_points
+
+  #Not sure if this is needed
+  accepts_nested_attributes_for :monitor_classes_monitor_points
+  accepts_nested_attributes_for :monitor_points, :allow_destroy => true
 end
