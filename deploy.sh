6a3984ba3318074c1fe6ec9cb56e580ba8ffdfe4
Trevor Ewen <tewen@neosavvy.com>
2013-11-27 11:00:57 -0500

    Added removal of bad bower components from the build.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,10 @@
 bundle install --deployment --without development test
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake db_inserts:all RAILS_ENV=production
+
+#Remove bad bower libraries, just a temp fix
+rm -r vendor/assets/bower_components/angular
+rm -r vendor/assets/javascripts/bower_components/angular
+
 bundle exec rake assets:precompile
 service apache2 restart
