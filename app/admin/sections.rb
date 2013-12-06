9e5d0767822aa34bfbcd848e2a6b3ced26e2e151
Trevor Ewen <tewen@neosavvy.com>
2013-12-05 20:44:13 -0500

    Disabled states are now supported for dropdowns.

--- app/admin/sections.rb
+++ app/admin/sections.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Section do
   menu :parent => 'Site Management'
 
+  form do |f|
+    f.inputs 'Section' do
+      f.input :name
+      f.input :location, :as => :select, :collection => Section.all
+      f.input :assets, :as => :select, :collection => Asset.all
+    end
+    f.actions
+  end
+
 end
