027699b64f7c83b33799c885d052debf2b5e8f2b
Trevor Ewen <tewen@neosavvy.com>
2013-12-29 19:05:30 -0500

    New deployment deletions.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,10 @@ bundle exec rake db_inserts:all RAILS_ENV=production
 #Remove bad bower libraries, just a temp fix
 rm -r vendor/assets/bower_components/angular
 rm -r vendor/assets/javascripts/bower_components/angular
+rm -r vendor/assets/bower_components/angular-route
+rm -r vendor/assets/javascripts/bower_components/angular-route
+rm -r vendor/assets/bower_components/handlebars
+rm -r vendor/assets/javascripts/bower_components/handlebars
 
 bundle exec rake assets:precompile
 service apache2 restart
