7d4b23441a052264eaa333450459d3c2e9ec21ad
Trevor Ewen <tewen@neosavvy.com>
2014-02-06 08:54:14 -0500

    Data table helper controller.

--- app/assets/javascripts/angular/controllers/helpers/data-table.js
+++ app/assets/javascripts/angular/controllers/helpers/data-table.js
@@ -2,12 +2,35 @@ RunEnergy.Dashboard.Controllers.controller('controllers.helpers.DataTable',
     ['$scope',
         function ($scope) {
 
-            $scope.getColumnLabel = memoize(function (key, monitorPoint, monitorLimit) {
+            if (!$scope.readingMods) {
+                $scope.readingMods = {
+                    deletedRowIndices: [],
+                    deletedColumns: {},
+                    columnToMonitorPointMappings: {},
+                    assetColumnName: null
+                };
+            }
+
+            //Action Handlers
+            $scope.onRemoveRow = function (index) {
+                var indexOf = $scope.readingMods.deletedRowIndices.indexOf(index);
+                if (indexOf === -1) {
+                    $scope.readingMods.deletedRowIndices.push(index);
+                } else {
+                    $scope.readingMods.deletedRowIndices.splice(indexOf, 1);
+                }
+            };
+
+            //Getters
+            $scope.getColumnLabel = memoize(function (key, monitorPoint) {
                 if (key && monitorPoint && monitorPoint.unit) {
-                    var val = key + " (" + monitorPoint.unit + ")";
-                    return _.isUndefined(monitorLimit) ? val : val + " Limit: " + monitorLimit;
+                    return key + " (" + monitorPoint.unit + ")";
                 }
                 return key;
             });
+
+            $scope.getRowVariation = function (index, optionA, optionB) {
+                return ($scope.readingMods.deletedRowIndices.indexOf(index) === -1 ? optionA : optionB);
+            };
 
         }]);
