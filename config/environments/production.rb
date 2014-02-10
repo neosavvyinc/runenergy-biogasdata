c373ae262da857419fd49d027d4f5769ed45713d
Trevor Ewen <tewen@neosavvy.com>
2014-02-09 23:20:49 -0500

    Updated production email config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,18 @@ Biogasdata::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # Don't care if the mailer can't send
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'tewen@neosavvy.com'}
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'runenergynotifications',
+      password:             'runenergy007',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # Enable threaded mode
   # config.threadsafe!
