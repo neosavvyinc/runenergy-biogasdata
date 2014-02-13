8ba27ca854903d4c3f7d5ed56ad9d4a7ec053f49
Trevor Ewen <tewen@neosavvy.com>
2014-02-13 15:51:58 -0500

    Proper CI build run.

--- ci_build.sh
+++ ci_build.sh
@@ -3,5 +3,7 @@
 bundle exec rake db:migrate
 bundle exec rake db:test:prepare
 bundle exec rspec
-karma start spec/javascripts/karma-ci.conf.js
+cd spec/javascripts
+npm install
+karma start karma-ci.conf.js
 
