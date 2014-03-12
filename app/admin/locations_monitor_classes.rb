dedb60b10da1f6a62e4b44c5431baf8ad33f731d
Trevor Ewen <tewen@neosavvy.com>
2014-03-12 09:00:22 -0400

    Fixed up the viewing for the locations monitor classes in the admin.

--- app/admin/locations_monitor_classes.rb
+++ app/admin/locations_monitor_classes.rb
@@ -6,6 +6,28 @@ ActiveAdmin.register LocationsMonitorClass, :as => 'Site Monitor Classes' do
     default_actions
   end
 
+  show do
+    panel 'Details' do
+      h6 site_monitor_classes.location.site_name
+      h6 site_monitor_classes.monitor_class.name
+    end
+    panel 'Field Log Points' do
+      site_monitor_classes.field_log_points.each do |flp|
+        h6 flp.name
+      end
+    end
+    panel 'Monitor Points' do
+      site_monitor_classes.monitor_points.each do |mp|
+        h6 "#{mp.name} (#{mp.units})"
+      end
+    end
+    panel 'Custom Monitor Calculations' do
+      site_monitor_classes.custom_monitor_calculations.each do |cmc|
+        h6 "#{cmc.name}: #{cmc.value}"
+      end
+    end
+  end
+
   form do |f|
     f.inputs 'Site Specific Assignments' do
       f.input :location, :as => :select, :collection => Location.all
