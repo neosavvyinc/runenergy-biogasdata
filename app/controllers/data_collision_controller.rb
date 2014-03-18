6ccdf40c3bcb06447ee8f36ae47fc293fda5f566
Trevor Ewen <tewen@neosavvy.com>
2014-03-18 17:44:53 -0400

    Data collision is rendering out, finally.

--- app/controllers/data_collision_controller.rb
+++ app/controllers/data_collision_controller.rb
@@ -10,9 +10,10 @@ class DataCollisionController < DataInterfaceController
         l.locations_monitor_classes.each do |lmc|
           @data_collision_blocks[lmc.display_name] = []
           Asset.where(:location_id => l, :monitor_class_id => lmc.monitor_class_id).each do |a|
-            @data_collision_blocks[lmc.display_name] << DataCollision.joins(:readings).where('readings.asset_id = ?', a.id)
+            @data_collision_blocks[lmc.display_name] = @data_collision_blocks[lmc.display_name].concat(DataCollision.joins(:readings).where('readings.asset_id = ?', a.id).map {|dc| dc})
           end
           @data_collision_blocks[lmc.display_name].uniq!
+          @data_collision_blocks[lmc.display_name].compact!
         end
       end
     else
