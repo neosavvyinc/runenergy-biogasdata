25e7b1cec7c08f938b81823d36082fec13ebe7af
Trevor Ewen <tewen@neosavvy.com>
2014-03-24 20:35:16 -0400

    Fixed the broken unit tests due to uids.

--- spec/factories/asset.rb
+++ spec/factories/asset.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :asset do |val|
 
+    sequence(:unique_identifier) {|n| "UID#{n}"}
+
     before :create do |asset|
       if asset.monitor_class.nil?
         asset.monitor_class = FactoryGirl.create(:monitor_class)
