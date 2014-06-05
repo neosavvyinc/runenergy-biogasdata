ec4e972fd84907ba045455e97807e413db6a9a97
Trevor Ewen <tewen@neosavvy.com>
2014-06-05 17:28:48 -0400

    Fixed up visualizations a bit to account for some more filters and take ranges into account.

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -4,7 +4,24 @@ class VisualizationsController < DataInterfaceController
 
   def monitor_point_progress
     @monitor_point = MonitorPoint.find(params[:monitor_point_id])
-    @readings = Reading.where(:location_id => params[:site], :monitor_class_id => params[:monitor_class]).map {
+    @readings = Reading.where(:location_id => params[:site], :monitor_class_id => params[:monitor_class])
+
+    #Asset and section optional filters
+    if params[:asset]
+        @readings = @readings.where(:asset_id => params[:asset])
+    elsif params[:section]
+      puts "Need a section filter"
+    end
+
+    #Start and end date optional filters
+    if params[:start]
+      @readings = @readings.where('taken_at >= ?', DateTime.strptime(params[:start].to_s, '%s'))
+    end
+    if params[:end]
+      @readings = @readings.where('taken_at <= ?', DateTime.strptime(params[:end].to_s, '%s'))
+    end
+
+    @readings = @readings.order('taken_at').map {
         |r| [(r.taken_at.try(:strftime, '%Y%m%d') || r.created_at.try(:strftime, '%Y%m%d')), JSON.parse(r.data)[@monitor_point.try(:name)]]
     }
   end
